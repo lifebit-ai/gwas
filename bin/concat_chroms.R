4b0e2a92eb6bfe3d0e3e9c3f0477505dfc4b9bff
cgpu <chatzipantsiou@gmail.com>
2020-08-14 08:15:45 +0100

    Adds library() calls to concat_chroms.R

--- bin/concat_chroms.R
+++ bin/concat_chroms.R
@@ -1,4 +1,8 @@
 #!/usr/bin/env Rscript
+
+library(plyr)
+library(data.table)
+
 paths <- list.files(".", pattern = "txt", full.names = TRUE)
 list_of_dfs <- lapply(paths,data.table::fread)
 analysis <- plyr::rbind.fill(list_of_dfs)
