d7d9229be1f4f4971e015f7d6f5492f45bdf64ab
cgpu <chatzipantsiou@gmail.com>
2020-08-14 08:29:21 +0100

    Adds mini script subset_gwascat.R [table]

---
+++ bin/subset_gwascat.R
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env Rscript
+
+library(data.table)
+
+a <- as.data.frame(data.table::fread("all.csv"))
+to_keep <- a[["SNPID"]]
+b <- as.data.frame(data.table::fread("gwascat.csv"))
+c <- b[ b$SNPS %in% to_keep, ]
+data.table::fread(c, file = "gwascat_subset.csv", sep = ",")
