4bc14fa0a389af337ad9c80e62cb81e11ed77745
cgpu <chatzipantsiou@gmail.com>
2020-08-14 12:02:59 +0000

    Corrects fread->fwrite

--- bin/subset_gwascat.R
+++ bin/subset_gwascat.R
@@ -7,4 +7,4 @@ a <- as.data.frame(data.table::fread("analysis.csv"))
 to_keep <- a[["SNPID"]]
 b <- as.data.frame(data.table::fread("gwascat.csv"))
 c <- b[ b$SNPS %in% to_keep, ]
-data.table::fread(c, file = "gwascat_subset.csv", sep = ",")
+data.table::fwrite(c, file = "gwascat_subset.csv", sep = ",")
