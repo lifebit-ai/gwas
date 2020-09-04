71a71868543f76b3f0fa3add49d2c3fac99d3766
cgpu <chatzipantsiou@gmail.com>
2020-09-04 12:13:16 +0000

    Updates DT table to display sci notation

--- bin/DTable.R
+++ bin/DTable.R
@@ -61,7 +61,16 @@ DTable <- function(df = df,
     # Change fontsize of cell values
     formatStyle(columns    = seq_along(colnames(df)), 
                 fontSize   = "85%",
-                fontFamily = "sans-serif")  -> fancyDatatable
+                fontFamily = "sans-serif")   %>%
+
+    formatSignif(
+      columns = unlist(lapply(df, is.numeric),
+      digits = 2,
+      interval = 3,
+      mark = ",",
+      dec.mark = getOption("OutDec")
+      )                 -> fancyDatatable
+
   
   return(fancyDatatable)  
 }
