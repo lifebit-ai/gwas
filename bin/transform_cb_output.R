dff31ca20ed33552d7a29089c11a8ccfdddad8c3
mcamarad <mcd95alm@gmail.com>
2020-11-19 23:51:38 +0000

    refactors support to NaNs in transform_cb_output.R

--- bin/transform_cb_output.R
+++ bin/transform_cb_output.R
@@ -162,7 +162,7 @@ encode_pheno_values = function(column, data, pheno_dictionary, transformation, a
         # Fill the gaps and get list of unique values
         pheno_cols[pheno_cols == ''] = "UNKNOWN"
         pheno_cols[is.na(pheno_cols)] = "UNKNOWN"
-        pheno_cols[is.nan(pheno_cols)] = "UNKNOWN"
+        pheno_cols[pheno_cols == NaN] = "UNKNOWN"
         
         pheno_values = pheno_cols %>% unlist() %>% sort() %>% unique()
         # Decide aggregation behaviour for samples with paired measures
