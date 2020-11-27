2f3d16b1643cacfe305992dbf7f700f39b99320f
mcamarad <mcd95alm@gmail.com>
2020-11-27 04:06:21 +0000

    Ensures all categoricals are strings and lowercase

--- bin/transform_cb_output.R
+++ bin/transform_cb_output.R
@@ -161,8 +161,10 @@ encode_pheno_values = function(column, data, pheno_dictionary, transformation, a
             pheno_cols = pheno_cols[[1]] %>% as.vector()
             return(pheno_cols)
         }
+        
+        # Ensure all categorical are strings and lowercase
+        pheno_cols = data.frame(lapply(pheno_cols, as.character %>% tolower), stringsAsFactors=FALSE)
         # Fill the gaps and get list of unique values
-        pheno_cols = data.frame(lapply(pheno_cols, as.character), stringsAsFactors=FALSE)
         pheno_cols[is.na(pheno_cols)] = ""
         pheno_cols[pheno_cols == NaN] = ""
         pheno_cols[pheno_cols == '<NA>'] = ""
